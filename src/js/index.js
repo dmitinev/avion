2ca5b3e44f5a5b3bbd83ac8a45ab4ec444d4619d
Dmitry Mitinev <83422156+dmitinev@users.noreply.github.com>
2023-09-25 19:01:13 +0200

    added burger menu style and more responsive styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
 import '../css/style.scss'
+
+const burgerBtn = document.querySelector(".header__burger");
+const navSection = document.querySelector(".header__navigation");
+
+function burgerHandler() {
+  navSection.classList.toggle("header__navigation--active");
+  burgerBtn.classList.toggle("header__burger--active");
+}
+
+burgerBtn.addEventListener("click", burgerHandler)
+
